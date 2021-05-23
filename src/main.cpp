a92e91078ca47822ff362fcbb8529299984d00f0
freol35241 <freol@outlook.com>
2021-05-23 18:50:29 +0200

    Switching CLI parser to CLI11. Seems to have a lot more functionality built-in.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,26 @@
 /// Copyright 2021 RISE Research Institute of Sweden. All rights reserved.
 #include <iostream>
 
+#include "CLI/CLI.hpp"
 #include "NMEA0183_assembler.hpp"
 #include "cluon-complete.hpp"
-#include "cxxopts.hpp"
 #include "spdlog/sinks/daily_file_sink.h"
 
 auto main(int argc, char **argv) -> int {
-  cxxopts::Options options("NMEA0183 Sentence Assembler",
-                           "Eavesdrops on a NMEA0183 stream and publishes each "
-                           "raw sentence to a running OpenDaVINCI session.");
+  CLI::App app{"NMEA0183 Eavesdropper"};
 
-  auto add = options.add_options();
-  add("c,cid", "OpenDaVINCI session id", cxxopts::value<int>());
-  add("a,address", "IP address of stream", cxxopts::value<std::string>());
-  add("p,port", "Port number to connect to", cxxopts::value<int>());
-  add("v,verbose", "Print to cout");
-  add("u,udp", "Use an UDP connection");
+  uint16_t cid;
+  app.add_option("-c,--cid", cid, "OpenDaVINCI session id")->required();
+  std::string address;
+  app.add_option("-a,--address", address, "IP address of stream")->required();
+  uint16_t port;
+  app.add_option("-p,--port", port, "Port number to connect to")->required();
+  bool use_udp = false;
+  app.add_option("--udp", use_udp, "Use an UDP connection");
+  bool verbose = false;
+  app.add_option("--verbose", verbose, "Print to cout");
 
-  auto arguments = options.parse(argc, argv);
-  const bool VERBOSE = arguments["verbose"].as<bool>();
-  const uint16_t cid = arguments["cid"].as<uint16_t>();
-  const std::string address = arguments["address"].as<std::string>();
-  const uint16_t port = arguments["port"].as<uint16_t>();
-  const bool use_udp = arguments["udp"].as<bool>();
+  CLI11_PARSE(app, argc, argv);
 
   // Setup a cluon instance
   cluon::OD4Session od4{cid,
@@ -31,12 +28,12 @@ auto main(int argc, char **argv) -> int {
 
   // A sink that rotates at midnight
   auto logger =
-      spdlog::daily_logger_mt("daily_logger", "NMEA0183/daily.txt", 0, 0);
+      spdlog::daily_logger_mt("daily_logger", "NMEA0183/sentences.txt", 0, 0);
   logger->set_pattern("%v");
 
   // Wrap everything in a sentence handler lambda
   auto sentence_handler =
-      [&VERBOSE, &logger, &od4](
+      [&verbose, &logger, &od4](
           const std::string &sentence,
           const std::chrono::system_clock::time_point &timestamp) {
         // TODO(freol35241): Publish message
@@ -46,7 +43,7 @@ auto main(int argc, char **argv) -> int {
         message << timestamp.time_since_epoch().count() << " " << sentence;
         logger->info(message.str());
 
-        if (VERBOSE) {
+        if (verbose) {
           std::cout << message.str() << std::endl;
         }
       };
@@ -54,6 +51,8 @@ auto main(int argc, char **argv) -> int {
   // And finally use this in a SentenceAssembler
   NMEA0183SentenceAssembler assembler(sentence_handler);
 
+  // Setup a connection to aither a udp or tcp stream with incoming NMEA0183
+  // messages
   if (use_udp) {
     cluon::UDPReceiver fromDevice{
         address, port,
@@ -62,7 +61,6 @@ auto main(int argc, char **argv) -> int {
           assembler(std::move(d), std::move(tp));
         }};
   } else {
-    // Interface to a TCP stream with incoming NMEA0183 messages
     cluon::TCPConnection stream{
         address, port, std::ref(assembler), [&argv]() {
           std::cerr << "[" << argv[0] << "] Connection lost." << std::endl;
