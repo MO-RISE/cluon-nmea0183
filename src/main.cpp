9764cbf5663531bfe7316934faac9764eab91929
Fredrik Olsson <freol@outlook.com>
2021-05-31 08:51:40 +0200

    Adding a risemo specific message set (for now) including a raw message type for nmea0183. Making sure the CMakeLists.txt sets up inclusion of libcluon-required third-party dependencies. Adding subcommand log in addition to gather. Manually tested to work across two instances.

--- src/main.cpp
+++ src/main.cpp
@@ -3,61 +3,11 @@
 
 #include "CLI/CLI.hpp"
 #include "NMEA0183_assembler.hpp"
+#include "cluon/Envelope.hpp"
 #include "cluon/OD4Session.hpp"
 #include "cluon/UDPReceiver.hpp"
-#include "opendlv-standard-message-set.hpp"
+#include "risemo-message-set.hpp"
 #include "spdlog/sinks/daily_file_sink.h"
-
-auto run_as_gatherer(uint16_t cid, uint16_t id, std::string address,
-                     uint16_t port, bool verbose) {
-  // Setup a cluon instance
-  cluon::OD4Session od4{cid,
-                        [](auto) {}};  // Empty callback for incoming messages.
-
-  // A sink that rotates at midnight
-  auto sink =
-      spdlog::daily_logger_mt("daily_logger", "NMEA0183/sentences.txt", 0, 0);
-  sink->set_pattern("%v");
-
-  // Wrap everything in a sentence handler lambda
-  auto sentence_handler =
-      [&verbose, &sink](
-          const std::string &sentence,
-          const std::chrono::system_clock::time_point &timestamp) {
-        // TODO(freol35241): Publish message on OpenDaVINCI session
-
-        // Log to disk
-        auto ms_since_epoch =
-            std::chrono::duration_cast<std::chrono::microseconds>(
-                timestamp.time_since_epoch())
-                .count();
-        std::stringstream message;
-        message << ms_since_epoch << " " << sentence;
-        sink->info(message.str());
-        sink->flush();
-
-        if (verbose) {
-          std::cout << message.str() << std::endl;
-        }
-      };
-
-  // And finally use this in a SentenceAssembler
-  NMEA0183SentenceAssembler assembler(sentence_handler);
-
-  // Setup a connection to an UDP source with incoming NMEA0183
-  // messages
-  cluon::UDPReceiver connection{
-      address, port,
-      [&assembler](std::string &&d, std::string && /*from*/,
-                   std::chrono::system_clock::time_point &&tp) noexcept {
-        assembler(d, std::move(tp));
-      }};
-
-  using namespace std::literals::chrono_literals;  // NOLINT
-  while (connection.isRunning()) {
-    std::this_thread::sleep_for(1s);
-  }
-}
 
 auto main(int argc, char **argv) -> int {
   CLI::App app{"NMEA0183 UDP Eavesdropper"};
@@ -76,15 +26,109 @@ auto main(int argc, char **argv) -> int {
   auto gather = app.add_subcommand(
       "gather",
       "Run in data gathering mode, i.e connect to a UDP stream, listen for "
-      "incoming NMEA0183 messages and publish them on an OD4 session");
+      "incoming NMEA0183 messages and publish them on an OD4 session. "
+      "Optionally, it is possible to dump the incoming NMEA0183 message "
+      "directly to disk");
   std::string address;
   gather->add_option("-a,--address", address, "IP address of stream")
       ->required();
   uint16_t port;
   gather->add_option("-p,--port", port, "Port number to connect to")
       ->required();
-  gather->callback(
-      [&]() { return run_as_gatherer(cid, id, address, port, verbose); });
+  bool standalone = false;
+  gather->add_flag("--standalone", standalone,
+                   "If application should be run in standalone mode, i.e. "
+                   "dumping any incoming NMEA0183 sentences directly to disk "
+                   "instead of publishing to an OD4 session");
+  gather->callback([&]() {
+    cluon::OD4Session od4{cid};
+    std::optional<std::shared_ptr<spdlog::logger>> sink = std::nullopt;
+
+    if (standalone) {
+      sink = spdlog::daily_logger_mt("daily_logger", "NMEA0183/sentences.txt",
+                                     0, 0);
+      (*sink)->set_pattern("%v");
+    }
+
+    // Wrap everything in a sentence handler lambda
+    auto sentence_handler =
+        [&od4, &id, &sink, &verbose](
+            const std::string &sentence,
+            const std::chrono::system_clock::time_point &tp) {
+          auto timestamp = cluon::time::convert(tp);
+
+          if (!sink.has_value()) {
+            // Publish to OD4 session
+            risemo::raw::NMEA0183 m;
+            m.sentence(sentence);
+            od4.send(m, timestamp, id);
+          } else {
+            // Log to disk
+            std::stringstream message;
+            message << cluon::time::toMicroseconds(timestamp) << " "
+                    << sentence;
+            (*sink)->info(message.str());
+            (*sink)->flush();
+          }
+
+          if (verbose) {
+            std::cout << sentence << std::endl;
+          }
+        };
+
+    // And finally use this in a SentenceAssembler
+    NMEA0183SentenceAssembler assembler(sentence_handler);
+
+    // Setup a connection to an UDP source with incoming NMEA0183
+    // messages
+    cluon::UDPReceiver connection{
+        address, port,
+        [&assembler](std::string &&d, std::string && /*from*/,
+                     std::chrono::system_clock::time_point &&tp) noexcept {
+          assembler(d, std::move(tp));
+        }};
+
+    using namespace std::literals::chrono_literals;  // NOLINT
+    while (connection.isRunning()) {
+      std::this_thread::sleep_for(1s);
+    }
+  });
+
+  // Log subcommand
+  auto log = app.add_subcommand(
+      "log",
+      "Run in data logging mode, i.e. connect to an OD4 session, listen for "
+      "all raw NMEA0183 sentences flying by, published by nodes in 'gather "
+      "mode' and dump these to disk");
+  log->callback([&]() {
+    // A sink that rotates at midnight
+    auto sink =
+        spdlog::daily_logger_mt("daily_logger", "NMEA0183/sentences.txt", 0, 0);
+    sink->set_pattern("%v");
+
+    // Setup a cluon instance
+    cluon::OD4Session od4{cid};
+    od4.dataTrigger(
+        risemo::raw::NMEA0183::ID(), [&](cluon::data::Envelope &&envelope) {
+          std::stringstream record;
+          record << cluon::time::toMicroseconds(envelope.sampleTimeStamp())
+                 << " " << envelope.senderStamp() << " "
+                 << cluon::extractMessage<risemo::raw::NMEA0183>(
+                        std::move(envelope))
+                        .sentence();
+          sink->info(record.str());
+          sink->flush();
+
+          if (verbose) {
+            std::cout << record.str() << std::endl;
+          }
+        });
+
+    using namespace std::literals::chrono_literals;  // NOLINT
+    while (od4.isRunning()) {
+      std::this_thread::sleep_for(1s);
+    }
+  });
 
   CLI11_PARSE(app, argc, argv);
 
